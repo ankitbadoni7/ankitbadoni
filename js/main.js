b8fc65f93f990215671cda867bc2c54a97ad0a54
ankit badoni <abankitcomputer@gmail.com>
2026-05-20 12:08:32 +0530

    from

--- js/main.js
+++ js/main.js
@@ -51,3 +51,43 @@ window.addEventListener("scroll", () => {
     });
 
 });
+
+
+
+// ===============================
+// EMAILJS CONTACT FORM SETUP
+// ===============================
+
+emailjs.init("j3L9lJOOKUcC-RMTR");
+
+const form = document.querySelector(".contact_form");
+
+const msg = document.createElement("p");
+msg.style.marginTop = "10px";
+msg.style.fontSize = "14px";
+msg.style.color = "#00ffcc";
+form.appendChild(msg);
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    msg.textContent = "Sending message...";
+
+    emailjs.sendForm(
+        "service_8weub3d",
+        "template_j1mynth",
+        this
+    )
+   .then(() => {
+    msg.textContent = "Message sent successfully ✔";
+    msg.style.color = "lightgreen";
+    msg.style.marginTop = "20px";
+    form.reset();
+})
+.catch((error) => {
+    console.log(error);
+    msg.textContent = "Failed to send message ❌";
+    msg.style.color = "red";
+    msg.style.marginTop = "20px";
+});
+});
